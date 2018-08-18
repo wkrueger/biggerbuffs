c35ef6f39b876618e15c445d3266791eed04157d
Willian Krueger <wkrueger128@gmail.com>
2018-08-18 13:32:41 -0300

    fix 8.0 unitbuff

--- MyBiggerBuffs.lua
+++ MyBiggerBuffs.lua
@@ -1,5 +1,49 @@
 BiggerBuffs = {}
 
+local UnitAura = UnitAura
+-- Unit Aura functions that return info about the first Aura matching the spellName or spellID given on the unit.
+local WA_GetUnitAura = function(unit, spell, filter)
+  for i = 1, 40 do
+    local name, _, _, _, _, _, _, _, _, spellId = UnitAura(unit, i, filter)
+    if not name then return end
+    if spell == spellId or spell == name then
+      return UnitAura(unit, i, filter)
+    end
+  end
+end
+
+local WA_GetUnitBuff = function(unit, spell, filter)
+  return WA_GetUnitAura(unit, spell, filter)
+end
+
+local WA_GetUnitDebuff = function(unit, spell, filter)
+  filter = filter and filter.."|HARMFUL" or "HARMFUL"
+  return WA_GetUnitAura(unit, spell, filter)
+end
+
+-- Function to assist iterating group members whether in a party or raid.
+local WA_IterateGroupMembers = function(reversed, forceParty)
+  local unit  = (not forceParty and IsInRaid()) and 'raid' or 'party'
+  local numGroupMembers = (forceParty and GetNumSubgroupMembers()  or GetNumGroupMembers()) - (unit == "party" and 1 or 0)
+  local i = reversed and numGroupMembers or (unit == 'party' and 0 or 1)
+  return function()
+    local ret
+    if i == 0 and unit == 'party' then
+      ret = 'player'
+    elseif i <= numGroupMembers and i > 0 then
+      ret = unit .. i
+    end
+    i = i + (reversed and -1 or 1)
+    return ret
+  end
+end
+
+-- Wrapping a unit's name in its class colour is very common in custom Auras
+local WA_ClassColorName = function(unit)
+  local _, class = UnitClass(unit)
+  if not class then return end
+  return RAID_CLASS_COLORS[class]:WrapTextInColorCode(UnitName(unit))
+end
 
 --split string (stack overflow)
 local function strsplit(inputstr, sep)
@@ -202,7 +246,7 @@ local function activateMe()
 				if BiggerBuffs.CDS[clazz] == nil then return end
 				while ( classBuffIdx <= #BiggerBuffs.CDS[clazz] ) do
 					local buffName,rank, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable
-						, shouldConsolidate,spellId = UnitBuff(frame.displayedUnit, BiggerBuffs.CDS[clazz][classBuffIdx]);
+						, shouldConsolidate,spellId = WA_GetUnitBuff(frame.displayedUnit, BiggerBuffs.CDS[clazz][classBuffIdx]);
 					classBuffIdx = classBuffIdx + 1;
 					if buffName ~= nil then
 						showBuff(buffFrame, icon, count, expirationTime, duration)
@@ -214,7 +258,7 @@ local function activateMe()
 				while ( additionalBuffIdx <= #additionalBuffs ) do
 					local buffName = additionalBuffs[additionalBuffIdx]
 					local buffName, rank, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable
-						, shouldConsolidate,spellId = UnitBuff(frame.displayedUnit, buffName);
+						, shouldConsolidate,spellId = WA_GetUnitBuff(frame.displayedUnit, buffName);
 					additionalBuffIdx  = additionalBuffIdx + 1
 					if buffName ~= nil and unitCaster == 'player' then
 						showBuff(buffFrame, icon, count, expirationTime, duration)
@@ -288,41 +332,15 @@ function frame:OnEvent(event, arg1)
 end
 
 
-local function showBuff(buffFrame, icon, count, expirationTime, duration)
-
-	--paste
-	buffFrame.icon:SetTexture(icon);
-	if ( count > 1 ) then
-		local countText = count;
-		if ( count >= 10 ) then
-			countText = BUFF_STACKS_OVERFLOW;
-		end
-
-		buffFrame.count:Show();
-		buffFrame.count:SetText(countText);
-	else
-		buffFrame.count:Hide();
-	end
-
-	if ( expirationTime and expirationTime ~= 0 ) then
-		local startTime = expirationTime - duration;
-		buffFrame.cooldown:SetCooldown(startTime, duration);
-		buffFrame.cooldown:Show();
-	else
-		buffFrame.cooldown:Hide();
-	end
-	buffFrame:Show();
-	--end paste
-
-end
-
 
 
 frame:SetScript("OnEvent", frame.OnEvent);
 
 
 BiggerBuffs.MY_ADDITIONAL_BUFFS = {
-	"Tranquil Mist"
+	"Tranquil Mist", -- monk T21
+	"Dreamer",        --druid T21,
+	"Highfather's Timekeeping" --trinket
 }
 
 
@@ -364,7 +382,8 @@ BiggerBuffs.CDS = {
 		"Guardian of the fortress"
 	} ,
 	[ 5 ] = { --priest
-		"Dispersion"
+		"Dispersion",
+		"Vestments of Discipline"
 	} ,
 	[ 4 ] = { --rogue
 		"Evasion",
